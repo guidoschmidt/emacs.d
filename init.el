8d1f7b072ad493ca84eb2ca7c7becca0cb1ef16d
Guido Schimdt <guido.schmidt.2912@gmail.com>
2017-03-06 16:59:29 +0100

    Add .gitignore, remove emacs toolbar

--- init.el
+++ init.el
@@ -15,7 +15,7 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages"))
+             '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 ;;; --- Bootstrap "use-package"
@@ -28,7 +28,7 @@
   :ensure t)
 
 ;;; --- Help on key combinations
-(use-package which-func-key
+(use-package which-key
   :ensure t
   :config (which-key-mode))
 
@@ -39,5 +39,24 @@
   :config
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
+
+;; Disable tool-bar
+(tool-bar-mode -1)
+
+
 (provide 'init.el)
 ;;; init.el ends here
+
+;;; --- Automatically added
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (org-bullets which-key use-package try))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
