da102eae784d01fe5aa773543942144138f3c63f
Guido Schmidt <guido.schmidt.2912@gmail.com>
2017-04-29 13:02:38 +0200

    Added dumb-jump

--- config/packages.el
+++ config/packages.el
@@ -67,6 +67,15 @@
   :config
   (counsel-projectile-on))
 
+;;; --- Dump-Jump
+(use-package dumb-jump
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :config (setq dumb-jump-selector 'ivy)
+  :ensure t)
+
 ;;; --- Undo-tree
 (use-package undo-tree
   :ensure t
