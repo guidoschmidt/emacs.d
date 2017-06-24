94a3805d7c456795d8b6942327c02c6266021ac2
Guido Schmidt <guido.schmidt.2912@gmail.com>
2017-06-25 00:09:22 +0200

    FEATURE: auto-complete for restclient with company-restclient

--- config/layers/auto-completion.el
+++ config/layers/auto-completion.el
@@ -12,17 +12,19 @@
   (use-package company-irony-c-headers :ensure t :defer t)
   (use-package company-gtags           :ensure t :defer t)
   (use-package company-tern            :ensure t :defer t)
+  (use-package company-restcliennt     :ensure t :defer t)
   (setq company-minimum-prefix-length 1
         company-show-numbers t
         company-tooltip-limit 20
         company-dabbrev-downcase nil
-        company-dabbrev-other-buffers t)
+        company-dabbrev-other-buffers t))
   ;;; Add backends
   (eval-after-load 'company
     (add-to-list 'company-backends (company-irony
                                     company-gtags
                                     company-tern
-                                    company-jedi)))
+                                    company-jedi
+                                    company-restclient)))
   :bind
   (("<C-tab>" . company-complete-common)))
 
