22be391de43cc951829bd8e0a25cdfe5a2b2e15e
Guido Schmidt <guido.schmidt.2912@gmail.com>
2017-07-24 21:17:05 +0200

    DEL: Removed ke bindings for font size changes in favor for universal arguments

--- config/keymap.el
+++ config/keymap.el
@@ -1,6 +1,7 @@
 ;;; keymap.el --- Configure custom key bindings
 ;;; Commentary:
 
+
 ;;; Code:
 ;; macOS key binding setup
 (setq mac-option-modifier nil
@@ -11,12 +12,11 @@
 (global-set-key (kbd "C-z") nil)
 
 ;; Custom key bindings
-(global-set-key (kbd "C-+")         'text-scale-increase)
-(global-set-key (kbd "C--")         'text-scale-decrease)
+;; (global-set-key (kbd "C- C-+") 'text-scale-increase)
+;; (global-set-key (kbd "C--")    'text-scale-decrease)
 (global-set-key (kbd "C-;")         'comment-or-uncomment-region)
 (global-set-key (kbd "C-M-f")       'toggle-frame-fullscreen)
 (global-set-key (kbd "C-c C-k")     'compile)
-(global-set-key (kbd "C-d")         'duplicate-line)
 (global-set-key (kbd "C-x g")       'goto-line)
 (global-set-key (kbd "M-<up>")      'move-line-up)
 (global-set-key (kbd "M-p")         'move-line-up)
