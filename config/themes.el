ebfae1c49a152eea0c76075ae18d898b2bbb0936
Guido Schmidt <guido.schmidt@immundiagnostik.com>
2017-09-18 10:43:48 +0200

    EDIT/themes.el: refactor themes.el config

--- config/themes.el
+++ config/themes.el
@@ -19,13 +19,7 @@
 ;; --- Theme load paths
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 
-;; --- Advice functions for theme loading
-(defun load-theme-disable-old-theme(theme &rest args)
-  "Disable current theme before loading new one."
-  (mapcar #'disable-theme custom-enabled-themes))
-(advice-add 'load-theme :before #'load-theme-disable-old-theme)
-
-;; Re-set linum after loading a new theme.
+;;; --- Re-set linum after loading a new theme.
 (defun load-theme-restore-line-numbering(theme &rest args)
   "Set appearance again after loading any theme."
   ;; Line numbers appearance
@@ -35,28 +29,17 @@
   (set-cursor-color "#FA0C72"))
 (advice-add 'load-theme :after #'load-theme-restore-line-numbering)
 
-;; --- Packaged themes from MELPA
-(use-package nyx-theme
-  :ensure t)
-
-(use-package hemera-theme
-  :ensure t)
-
-(use-package spacegray-theme
-  :ensure t)
-
-(use-package soothe-theme
-  :ensure t)
-
 ;; --- Circadian
 (use-package circadian
+  :load-path "~/git/misc/emacs/circadian.el"
   :ensure t
+  :init
+  (use-package nyx-theme :ensure t)
+  (use-package hemera-theme :ensure t)
   :config
-  (setq circadian-themes '((" 8:00" . hemera)
-                           ("14:00" . nyx)
-                           ("20:00" . soothe)))
+  (setq circadian-themes '(("8:00" . hemera)
+                           ("20:00" . nyx)))
   (circadian-setup))
-
 
 (provide 'themes)
 ;;; themes.el ends here
