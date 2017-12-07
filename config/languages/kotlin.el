70b778e4672eadb1c6c8d073109dd990189fbc2b
Guido Schmidt <guido.schmidt@immundiagnostik.com>
2017-12-07 11:44:39 +0100

    FIX/kotlin.el: fix kotlin mode autoload for .kt files

--- config/languages/kotlin.el
+++ config/languages/kotlin.el
@@ -4,19 +4,19 @@
 ;;; Code:
 (use-package kotlin-mode
   :ensure
-  :defer
   :mode "\\.kt\\'"
   :init
-  (add-to-list 'auto-mode-alist '("\\.kt$" . kotlin-mode)))
+  (add-to-list 'auto-mode-alist '("\\.kt\\'" . kotlin-mode)))
 
-(use-package flycheck-kotlin
-  :ensure
-  :defer
-  :mode "\\.kt\\'"
-  :after (flycheck)
-  :config
-  (eval-after-load 'flycheck
-    (flycheck-kotlin-setup)))
+;; (use-package flycheck-kotlin
+;;   :ensure
+;;   :defer
+;;   :commands (kotlin-mode)
+;;   :mode "\\.kt\\'"
+;;   :after (flycheck kotlin-mode)
+;;   :config
+;;   (eval-after-load 'flycheck
+;;     (flycheck-kotlin-setup)))
 
 (provide 'kotlin.el)
 ;;; kotlin.el ends here
