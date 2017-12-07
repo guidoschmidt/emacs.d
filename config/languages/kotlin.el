820b816b67c650c18299674f8265303dd9198054
Guido Schmidt <guido.schmidt@immundiagnostik.com>
2017-12-07 08:46:40 +0100

    ADD/kotlin.el: configure kotlin support

--- config/languages/kotlin.el
+++ config/languages/kotlin.el
@@ -5,12 +5,18 @@
 (use-package kotlin-mode
   :ensure
   :defer
-  :mode "\\.kt\\'")
+  :mode "\\.kt\\'"
+  :init
+  (add-to-list 'auto-mode-alist '("\\.kt$" . kotlin-mode)))
 
 (use-package flycheck-kotlin
   :ensure
   :defer
-  :mode "\\.kt\\'")
+  :mode "\\.kt\\'"
+  :after (flycheck)
+  :config
+  (eval-after-load 'flycheck
+    (flycheck-kotlin-setup)))
 
 (provide 'kotlin.el)
 ;;; kotlin.el ends here
