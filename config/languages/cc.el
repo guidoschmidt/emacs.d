c9f505397884bfecd8eea0ffc8f3dc5394ba062f
Guido Schmidt <git@guidoschmidt.cc>
2018-01-07 19:53:05 +0100

    ADD/ycmd: add submodule for ycmd + ycm_extra_conf.py

--- config/languages/cc.el
+++ config/languages/cc.el
@@ -2,20 +2,21 @@
 
 ;;; Commentary:
 ;;; Setup C/C++ features with
-;;; - CMake
 ;;; - CTAGS & GTAGS
-;;; - Irony
+;;; - YouCompleteMe
 
 ;;; Code:
+
+;;; --- Enable c++-mode for .h and .hpp files
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 (add-to-list 'auto-mode-alist '("\\.hpp\\'" . c++-mode))
 
 ;;; --- CMake
-(use-package cmake-ide
-  :ensure
-  :defer
-  :config
-  (cmake-ide-setup))
+;; (use-package cmake-ide
+;;   :ensure
+;;   :defer
+;;   :config
+;;   (cmake-ide-setup))
 
 ;;; --- Modern C++11 font lock/syntax highlighting
 (use-package modern-cpp-font-lock
@@ -35,52 +36,15 @@
       (ggtags-mode 1)))
   (add-hook 'c-mode-common-hook 'ggtags/c-mode-hook))
 
-(use-package flycheck-irony
-  :ensure
-  :defer
-  :config
-  (eval-after-load 'flycheck
-    '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup)))
+;; --- clang-format
+(use-package clang-format
+  ;; Create clang-format file using google style
+  ;; clang-format -style=google -dump-config > .clang-format
+  :ensure t
+  :bind
+  (("C-c C-f" . clang-format-region)))
 
-;;; --- Irony - C++ IDE
-(use-package irony
-  :ensure
-  :defer
-  :config
-  (use-package company-irony :ensure t)
-  (use-package company-irony-c-headers :ensure t)
-  (use-package irony-eldoc :ensure t)
-  (custom-set-variables
-   '(irony-additional-clang-options
-     '("-I/Library/Developer/CommandLineTools/usr/include/c++/v1")))
-  (defun custom/irony-mode-hook ()
-    "Hook to customize irony mode."
-    (add-to-list 'company-backends 'company-irony)
-    (add-to-list 'company-backends 'company-irony-c-headers)
-    (add-to-list 'company-backends 'company-dabbrev)
-    (define-key irony-mode-map [remap completion-at-point] 'counsel-irony)
-    (define-key irony-mode-map [remap complete-symbol] 'counsel-irony))
-  (add-hook 'irony-mode-hook 'custom/irony-mode-hook)
-  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-  (defun cc/irony-mode-hook ()
-    (unless (glsl-mode)
-      (irony-mode)
-      (add-hook 'irony-mode-hook #'irony-eldoc)))
-  (add-hook  'c-mode-hook '    cc/irony-mode-hook)
-  (add-hook  'c++-mode-hook '  cc/irony-mode-hook)
-  (add-hook  'objc-mode-hook ' cc/irony-mode-hook))
-
-;; --- astyle
-(defun astyle-this-buffer (pmin pmax)
-  "Astyle a buffer region from PMIN to PMAX."
-  (interactive "r")
-  (shell-command-on-region
-   pmin pmax
-   "astyle --style=google -s2"
-   (current-buffer) t
-   (get-buffer-create "*Astyle Errors*") t))
-
-;; Add Google C++ style
+;; -- Add Google C++ style
 (use-package google-c-style
   :ensure t
   :defer
@@ -89,7 +53,7 @@
   (add-hook 'c++-mode-hook 'google-set-c-style)
   (add-hook 'c-mode-hook 'google-set-c-style))
 
-;; Add Google C++ style checker - In default, syntax checked by clang
+;; -- Add Google C++ style checker - In default, syntax checked by clang
 (use-package flycheck-google-cpplint
   :load-path "config/layers/"
   :defer
@@ -97,6 +61,45 @@
   (eval-after-load 'flycheck
     '(progn (flycheck-add-next-checker 'c/c++-clang
                                        '(warning . c/c++-googlelint)))))
+
+;; -- YouCompleteMe
+(use-package ycmd
+  :ensure
+  :commands c++-mode
+  :init
+  (add-hook 'c++-mode-hook 'ycmd-mode)
+  :config
+  (set-variable 'ycmd-global-config "/Users/gs/.emacs.d/ycm_extra_conf.py")
+  (set-variable 'ycmd-server-command '("python" "/Users/gs/.emacs.d/ycmd/ycmd"))
+  (setq ycmd-force-semantic-completion t))
+
+(use-package company-ycmd
+  :ensure
+  :config (company-ycmd-setup))
+
+(use-package flycheck-ycmd
+  :ensure
+  :config (flycheck-ycmd-setup))
+
+;;; -- Counsel with etags
+;;; TODO: move to it's code-navigation package
+(use-package counsel-etags
+  :ensure
+  :config
+  ;; Don't ask before rereading the TAGS files if they have changed
+  (setq tags-revert-without-query t)
+  ;; Don't warn when TAGS files are large
+  (setq large-file-warning-threshold nil)
+  ;; Setup auto update now
+  (add-hook 'prog-mode-hook
+            (lambda ()
+              (add-hook 'after-save-hook
+                        'counsel-etags-virtual-update-tags 'append 'local)))
+  :bind
+  (("C-1" . counsel-etags-find-tag-at-point)
+   ("C-0" . xref-pop-marker-stack)
+   ("C-2" . counsel-etags-grep-symbol-at-point)
+   ("C-3" . counsel-etags-find-tag)))
 
 (provide 'cc.el)
 ;;; cc.el ends here
