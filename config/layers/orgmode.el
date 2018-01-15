ab769ab7314933b9eb1501e1af5da6eeb7fd356b
Guido Schmidt <guido.schmidt@immundiagnostik.com>
2018-01-15 08:03:04 +0100

    EDIT/orgmode.el: customize org-todo-keywords

--- config/layers/orgmode.el
+++ config/layers/orgmode.el
@@ -11,8 +11,19 @@
   (setq org-bullets-bullet-list '("☰" "➟" "●" "✕"))
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
-;; enable syntax highlighting by default
+;; -- Enable syntax highlighting by default
 (setq org-src-fontify-natively t)
+
+;; --- Setup TODO states
+(setq org-todo-keywords
+      '((sequence "IN" "TODO" "BLOCKED" "WIP" "|" "DONE" "WONTDO")))
+(setq org-todo-keyword-faces
+      '(("IN"      . (:foreground "white"  :weight bold))
+        ("TOOD"    . (:foreground "orange" :weight bold))
+        ("BLOCKED" . (:foreground "red"    :weight bold))
+        ("WIP"     . (:foreground "orange" :weight bold))
+        ("DONE"    . (:foreground "green"  :weight bold))
+        ("WONTDO"  . (:foreground "green"  :weight bold))))
 
 (load "~/.emacs.d/github/org-kanban/org-kanban.el")
 
