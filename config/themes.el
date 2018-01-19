35757e4f64d667dae14105837ebedf31797d294d
Guido Schmidt <guido.schmidt@immundiagnostik.com>
2018-01-19 16:35:06 +0100

    EDIT/themes.el: change daytime theme to gruvbox-light-hard

--- config/themes.el
+++ config/themes.el
@@ -42,7 +42,7 @@
   :config
   (setq calendar-latitude 49.329896)
   (setq calendar-longitude 8.570925)
-  (setq circadian-themes '((:sunrise . apropospriate-dark)
+  (setq circadian-themes '((:sunrise . gruvbox-light-hard)
                            (:sunset . ample-flat)))
   (circadian-setup)
   (add-hook 'after-init-hook 'circadian-setup))
