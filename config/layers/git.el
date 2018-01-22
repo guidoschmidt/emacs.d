7fa59fbd6a1a2280287b0706a064e94988ca9271
Guido Schmidt <guido.schmidt@immundiagnostik.com>
2018-01-22 08:24:47 +0100

    EDIT/git.el: use ssh-agency only for windows

--- config/layers/git.el
+++ config/layers/git.el
@@ -24,11 +24,12 @@
   (custom-set-variables
    '(git-gutter:update-interval 2)))
 
-(use-package ssh-agency
-  :ensure
-  :config
-  (ssh-agency-find-agent)
-  (ssh-agency-add-keys "~/.ssh/id_rsa.github.cube"))
+(when (memq window-system '(w32))
+  (use-package ssh-agency
+    :ensure
+    :config
+    (ssh-agency-find-agent)
+    (ssh-agency-add-keys "~/.ssh/id_rsa.github.cube")))
 
 (provide 'git.el)
 ;;; git.el ends here
