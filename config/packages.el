11e2cf724eeb9bd1f00c5fa3761b51cdcb7f2123
Guido Schmidt <git@guidoschmidt.cc>
2018-01-28 22:38:11 +0100

    ADD/modeline.el: move modeline/spaceline config to separate file

--- config/packages.el
+++ config/packages.el
@@ -86,112 +86,16 @@
   (setq highlight-indent-guides-method 'character)
   (add-hook 'prog-mode-hook 'highlight-indent-guides-mode))
 
-;;; --- Powerline & Spaceline
-(use-package sky-color-clock
-  :load-path "~/.emacs.d/github/sky-color-clock"
-  :config
-  (when calendar-latitude
-   (sky-color-clock-initialize (round calendar-latitude))))
-
-(use-package powerline
-  :ensure)
-
-(use-package fancy-battery
-  :ensure)
-
-(use-package spaceline
-  :ensure
-  :after powerline
-  :init
-  (progn
-    (require 'spaceline-config)
-    ;; -- Powerline seperator styles:
-    ;; alternate, arrow, arrow-fade, bar, box, brace,
-    ;; butt, chamfer, contour, curve, rounded, roundstub,
-    ;; wave, zigzag, utf-8, nil
-    (setq powerline-default-separator nil)
-    (setq powerline-height 42)
-    (setq powerline-gui-use-vcs-glyph t)
-    ;; -- Disable spaceline segments
-    ;; (spaceline-toggle-workspace-number-off)
-    ;; (spaceline-toggle-minor-modes-off)
-    ;; (spaceline-toggle-buffer-encoding-abbrev-off)
-    ;; (spaceline-toggle-buffer-size-off)
-    ;; (spaceline-toggle-org-clock-off)
-    ;; (spaceline-toggle-projectile-root-off)
-    ;; (spaceline-toggle-battery-off)
-    ;; (spaceline-toggle-selection-info-off)
-    ;; (spaceline-toggle-evil-state-off)
-    ;; (spaceline-toggle-buffer-id-off)
-    ;; (spaceline-toggle-major-mode-off)
-    ;; (spaceline-toggle-minor-modes-off)
-    ;; (spaceline-toggle-flycheck-error-off)
-    ;; (spaceline-toggle-flycheck-info-off)
-    ;; (spaceline-toggle-flycheck-warning-off)
-    ;; (spaceline-toggle-version-control-off)
-    ;; (spaceline-toggle-line-column-off)
-    ;; (spaceline-toggle-global-off)
-    ;; (spaceline-toggle-hud-off)
-    ;; (spaceline-toggle-buffer-position-off)
-    ;; (spaceline-toggle-buffer-modified-off)
-    )
-  :config
-  (spaceline-emacs-theme))
-
-(use-package all-the-icons
-  :load-path "~/.emacs.d/github/all-the-icons"
-  :init
-  (when (memq window-system '(w32))
-    (add-to-list 'load-path "~/.emacs.d/github/")
-    (require 'font-lock+)))
-
-(use-package spaceline-all-the-icons
-  :load-path "~/.emacs.d/github/spaceline-all-the-icons"
-  :after (spaceline all-the-icons)
-  :config
-  ;; -- Customize spaceline
-  (set-face-attribute 
-   'spaceline-evil-normal nil :background "#fafefd" :foreground "#232323")
-  (set-face-attribute
-   'spaceline-evil-insert nil :background "#3bffde" :foreground "#232323")
-  (set-face-attribute
-   'spaceline-evil-visual nil :background "#e012a0" :foreground "#fafefd")
-  (setq spaceline-all-the-icons-separator-type 'none)
-  (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
-  ;; -- Define custom segments
-  (spaceline-define-segment sky-color-clock-segment
-    (concat "" (sky-color-clock))
-    :tight t)
-  ;; -- Turn segemnts off
-  (spaceline-toggle-all-the-icons-buffer-path-off)
-  (spaceline-toggle-all-the-icons-buffer-position-off)
-  (spaceline-toggle-all-the-icons-buffer-size-off)
-  (spaceline-toggle-all-the-icons-hud-off)
-  (spaceline-toggle-all-the-icons-modified-off)
-  (spaceline-toggle-all-the-icons-narrowed-off)
-  (spaceline-toggle-all-the-icons-projectile-off)
-  (spaceline-toggle-all-the-icons-region-info-off)
-  (spaceline-toggle-all-the-icons-time-off)
-  ;; -- Turn segments on
-  (spaceline-toggle-all-the-icons-buffer-id-on)
-  (spaceline-toggle-all-the-icons-flycheck-status-on)
-  (spaceline-toggle-all-the-icons-git-status-on)
-  (spaceline-toggle-all-the-icons-mode-icon-on)
-  (spaceline-toggle-all-the-icons-position-on)
-  (spaceline-toggle-all-the-icons-package-updates-on)
-  (spaceline-toggle-all-the-icons-minor-modes-on)
-  (spaceline-all-the-icons-theme
-   'sky-color-clock-segment
-   'etc))
-
 ;;; --- Neo-tree with icons
 (use-package neotree
   :ensure
   :commands neotree-toggle
   :config
-  (use-package all-the-icons
-    :ensure)
-  (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
+  (custom-set-faces
+   '(neo-dir-link-face ((t (:weight bold :height 100))))
+   '(neo-file-link-face ((t (:weight normal :height 110))))
+   '(neo-banner-face ((t :height 100))))
+  (setq neo-theme (if (display-graphic-p) 'icons)))
 
 ;;; --- Rainbow-delimiters
 (use-package rainbow-delimiters
@@ -236,7 +140,8 @@
 ;;; --- Auto highlight words
 (use-package auto-highlight-symbol
   :ensure
-  :config (global-auto-highlight-symbol-mode t))
+  :config
+  (global-auto-highlight-symbol-mode t))
 
 ;;; --- Exec-path-from-shell
 (use-package exec-path-from-shell
@@ -246,10 +151,6 @@
     (setq explicit-shell-file-name "/bin/zsh")
     (setq shell-file-name "zsh")
     (exec-path-from-shell-initialize)))
-
-(when (memq window-system '(w32))
-  (print "WIND"))
-
 
 ;;; --- Setup ace-window
 (use-package ace-window
@@ -370,6 +271,7 @@
 (load "~/.emacs.d/config/layers/shell.el")
 (load "~/.emacs.d/config/layers/spell-checking.el")
 (load "~/.emacs.d/config/layers/syntax-checking.el")
+(load "~/.emacs.d/config/modeline.el")
 (load "~/.emacs.d/config/notifications.el")
 
 ;;; --- Languages setup
