759d70921eb314aeeed650e671a9fa5d5d981ab3
Guido Schmidt <git@guidoschmidt.cc>
2018-01-28 22:48:34 +0100

    EDIT/python.el: ensure python3 in exec-path on windows

--- config/languages/python.el
+++ config/languages/python.el
@@ -4,8 +4,11 @@
 ;;; Code:
 ;;; --- Python
 (use-package elpy
+  :ensure
   :commands python-mode
   :config
+  (when (memq window-system '(w32))
+    (setq exec-path (append exec-path '("C:/Development/python3/"))))
   (setq elpy-rpc-python-command "python3")
   (setq python-shell-interpreter "python3")
   (defun custom-python-mode-hook ()
@@ -16,9 +19,12 @@
   (add-hook 'python-mode-hook 'custom-python-mode-hook))
 
 (use-package company-jedi
-  :ensure t
+  :ensure
+  :commands python-mode
   :config
-  (defun company/python-mode-hook ()
+  (when (memq window-system '(w32))
+    (setq exec-path (append exec-path '("c:/Development/python3"))))
+ (defun company/python-mode-hook ()
     (push 'company-jedi company-backends))
   (add-hook 'python-mode-hook 'company/python-mode-hook))
 
