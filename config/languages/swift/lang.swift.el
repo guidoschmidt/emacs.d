f3d59fb66fe48c366f3be8cc5f534b3c78e8b2f1
Guido Schmidt <guido.schmidt@immundiagnostik.com>
2018-02-20 10:21:14 +0100

    FIX/lang.swift.el: fix flycheck-swift and swift mode

--- config/languages/swift/lang.swift.el
+++ config/languages/swift/lang.swift.el
@@ -8,19 +8,17 @@
   :mode "\\.swift\\'")
 
 (use-package flycheck-swift3
-  :mode "\\.swift\\'"
   :ensure t
+  :after (flycheck)
   :config
-  (with-eval-after-load 'flycheck
-    (add-hook 'flycheck-mode-hook #'flycheck-swift3-setup))
+  (add-hook 'flycheck-mode-hook #'flycheck-swift3-setup)
   (setq flycheck-swift-sdk-path
         "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS10.0.sdk")
   ;; ↑ Select the appropriate SDK version you use
   (setq flycheck-swift-target "arm64-apple-ios10"))
 
 (use-package company-sourcekit
-  :mode "\\.swift\\'"
-  :ensure
+  :ensure t
   :config
   (defun company/swift-mode-hook ()
     (add-to-list 'company-backends '(company-dabbrev :with company-sourcekit))
