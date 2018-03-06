d673046c8f03a09e4e37c4d80b6c86811ef49d68
Guido Schmidt <git@guidoschmidt.cc>
2018-03-06 23:28:33 +0100

    EDIT/init.el: move editor.languages + editor.layers requires to init.el

--- init.el
+++ init.el
@@ -11,7 +11,9 @@
 ;; You may delete these explanatory comments.
 ;;(package-initialize)
 
-;; Setup the load path
+(setq custom-file "~/.emacs.d/local/custom-set.el")
+
+; Setup the load path
 (let ((default-directory  "~/.emacs.d/config/"))
   (normal-top-level-add-to-load-path '("."))
   (normal-top-level-add-subdirs-to-load-path))
@@ -21,11 +23,42 @@
 (require 'private.keys)
 (require 'editor.packages)
 (require 'editor.keymap)
-(require 'editor.appearance)
-(require 'editor.layers)
-(require 'editor.languages)
 
-(setq custom-file "~/.emacs.d/local/custom-set.el")
+;; Appearance
+(require 'editor.appearance)
+(require 'ligatures)
+(require 'modeline)
+(require 'themes)
+
+;; Layers
+(require 'layer.codecompletion.auto-complete)
+(require 'layer.codecompletion.company)
+(require 'layer.codenavigation)
+(require 'layer.evil)
+(require 'layer.git)
+(require 'layer.notifications)
+(require 'layer.org)
+(require 'layer.restclient)
+(require 'layer.shell)
+(require 'layer.spellcheck)
+(require 'layer.syntaxcheck)
+
+;; Languages
+(require 'lang.android)
+(require 'lang.arduino)
+(require 'lang.cpp)
+(require 'lang.clojure)
+(require 'lang.common-lisp)
+(require 'lang.css)
+(require 'lang.emacs-lisp)
+(require 'lang.glsl)
+(require 'lang.haskell)
+(require 'lang.javascript)
+(require 'lang.kotlin)
+(require 'lang.markup)
+(require 'lang.php)
+(require 'lang.python)
+(require 'lang.swift)
 
 (provide 'init.el)
 ;;; init.el ends here
