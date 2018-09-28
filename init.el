2c72440b46693a1679fbf0904637908543817b9e
Guido Schmidt <guido.schmidt@immundiagnostik.com>
2018-09-28 16:20:26 +0200

    EDIT/init.el: remove package-initialize

--- init.el
+++ init.el
@@ -4,13 +4,6 @@
 ;;; Load configuration files from .emacs.d/config/
 
 ;;; Code:
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-;;(package-initialize)
-
 ;; Avoid garbage collection during startup
 (setq gc-cons-threshold 402653184
       gc-cons-percentage 0.6)
