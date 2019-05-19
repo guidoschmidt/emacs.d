85124b916fde8508281118bd07bfa6c812ca3251
Guido Schmidt <git@guidoschmidt.cc>
2019-05-19 23:40:09 +0200

    FIX/config/editor.keymap.el: clean up keymap

--- config/editor.keymap.el
+++ config/editor.keymap.el
@@ -16,22 +16,24 @@
 
 ;; Custom key bindings
 (global-set-key (kbd "M-;")         'comment-or-uncomment-region)
+
 (global-set-key (kbd "C-c C-k")     'compile)
-(global-set-key (kbd "C-x g")       'goto-line)
-(global-set-key (kbd "M-<up>")      'move-line-up)
+
 (global-set-key (kbd "M-p")         'move-line-up)
-(global-set-key (kbd "M-<down>")    'move-line-down)
 (global-set-key (kbd "M-n")         'move-line-down)
+
 (global-set-key (kbd "RET")         'newline-and-indent)
+
 (global-set-key (kbd "C-c s")       'flyspell-popup-correct)
+
 (global-set-key (kbd "C-c x")       'insert-checkmark)
+
 (global-set-key (kbd "S-C-<left>")  'shrink-window-horizontally)
 (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "S-C-<down>")  'shrink-window)
 (global-set-key (kbd "S-C-<up>")    'enlarge-window)
+
 (global-set-key (kbd "C-x n")       'new-buffer)
-(global-set-key (kbd "C-c t")       'neotree-toggle)
-(global-set-key (kbd "C-c s")       'magit-status)
 
 (provide 'editor.keymap)
 ;;; editor.keymap ends here
