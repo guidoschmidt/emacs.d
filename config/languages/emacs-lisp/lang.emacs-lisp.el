8aff0f4d68ceddea908c98ba338a3eafc7b1393b
Guido Schmidt <git@guidoschmidt.cc>
2020-05-01 11:42:56 +0200

    EDIT/lang.emacs-lisp.el: remove commented code

--- config/languages/emacs-lisp/lang.emacs-lisp.el
+++ config/languages/emacs-lisp/lang.emacs-lisp.el
@@ -1,15 +1,7 @@
-;;; lang.emacs-lisp --- Configure elisp
+;;; lang.emacs-lisp --- Configure Emacs lisp
 ;;; Commentary:
 
 ;;; Code:
-;; (autoload 'enable-paredit-mode "paredit"
-;;   "Turn on pseudo-structural editing of Lisp code." t)
-;; (add-hook 'emacs-lisp-mode-hook                  #'enable-paredit-mode)
-;; (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-;; (add-hook 'ielm-mode-hook                        #'enable-paredit-mode)
-;; (add-hook 'lisp-mode-hook                        #'enable-paredit-mode)
-;; (add-hook 'lisp-interaction-mode-hook            #'enable-paredit-mode)
-;; (add-hook 'scheme-mode-hook                      #'enable-paredit-mode)
 
 (provide 'lang.emacs-lisp)
 ;;; lang.emacs-lisp ends here
