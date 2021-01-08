80787558f629cdcd5b21fc11caa13d33f3385429
Guido Schmidt <git@guidoschmidt.cc>
2021-01-09 00:44:56 +0100

    EDIT/init.el: yasnippet + snippets/ git submodule, evil improvements
    + avy
    + ivy
    + frog-jump-buffer
    + custom functions

---
+++ core/core.functions.el
@@ -0,0 +1,33 @@
+;;; core.functions.el --- Custom functions for Emacs -*- lexical-binding: t; -*-
+(defun duplicate-line ()
+  "Duplicate a line."
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line)
+  (yank)
+  (forward-line 1)
+  (yank))
+
+(defun move-line-up ()
+  "Move the active line upwards."
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2))
+
+(defun move-line-down ()
+  "Move the active line downwards."
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1))
+
+(defun reload-current-buffer ()
+  "Reload the file loaded in the current buffer from disk."
+  (interactive)
+  (cond (buffer-file-name
+         (progn (find-alternate-file buffer-file-name)
+                (message "File reloaded.")))
+        (t (message "Buffer has no file attached to reload."))))
+
+(provide 'core.functions)
+;;; core.functions.el ends here
--- init.el
+++ init.el
@@ -3,7 +3,6 @@
 ;;; Commentary:
 ;;; Load configuration files from .emacs.d/config/
 
-
 ;; A big contributor to startup times is garbage collection. We up the gc
 ;; threshold to temporarily prevent it from running, then reset it later by
 ;; enabling `gcmh-mode'. Not resetting it will cause stuttering/freezes.
@@ -38,6 +37,9 @@
 ;; yes-or-no shortcut for dialogues
 (defalias 'yes-or-no-p 'y-or-n-p)
 
+;; Define a file used for storing customization information.
+(setq custom-file "~/.emacs.d/local/custom.el")
+
 ;; Time display
 (display-time-mode t)
 
@@ -58,6 +60,15 @@
 ;; Set window fringe
 (fringe-mode 20)
 
+;; Enable winner mode for window history
+(winner-mode t)
+
+;; Disable visual bell
+(setq ring-bell-function 'ignore)
+(setq echo-keystrokes 0.1
+      use-dialog-box nil
+      visible-bell t)
+
 ;; Allow pixelwise frame sizing
 (setq frame-resize-pixelwise t)
 
@@ -67,12 +78,17 @@
 
 ;; Move backup files
 (setq backup-directory-alist
-      `((".*" "~/.emacs.d/backup/" t)))
+      '((".*" "~/.emacs.d/backup/")))
 (setq backup-by-copying t)
 (setq delete-old-versions t
       kept-new-versions 6
       kept-old-versions 2
       version-control t)
+(setq make-backup-files nil)
+
+;; When delete-selection-mode is active, typed or pasetd text
+;; will delete a selcted text region
+(delete-selection-mode t)
 
 ;; Move auto-save files
 (setq auto-save-file-name-transforms
@@ -112,6 +128,9 @@
   (add-to-list 'recentf-exclude no-littering-var-directory)
   (add-to-list 'recentf-exclude no-littering-etc-directory))
 
+;; Diminish
+(use-package diminish
+  :straight t)
 
 ;; evil-mode
 (use-package evil-leader
@@ -120,21 +139,25 @@
   (global-evil-leader-mode)
   (evil-leader/set-leader "<SPC>")
   (evil-leader/set-key
-    "a" 'align-regexp
-    "o" 'sort-lines
-    "b" 'ivy-switch-buffer
-    "i" 'ibuffer
-    "n" 'ivy-switch-buffer-other-window
-    "k" 'ido-kill-buffer
-    "s" 'magit-status
-    "p" 'counsel-projectile-switch-project
-    "f" 'counsel-projectile-find-file
-    "g" 'counsel-ag
-    "w" 'save-buffer
-    "j" 'swiper-avy
-    "?" 'flyspell-correct-at-point
-    "!" 'flyspell-add-word-to-dict
-    "0" 'switch-window
+    "a"   'align-regexp
+    "o"   'sort-lines
+    "b"   'ivy-switch-buffer
+    "x"   'frog-jump-buffer
+    "i"   'ibuffer
+    "n"   'ivy-switch-buffer-other-window
+    "k"   'ido-kill-buffer
+    "s"   'magit-status
+    "p"   'counsel-projectile-switch-project
+    "f"   'counsel-projectile-find-file
+    "g"   'counsel-ag
+    "w"   'save-buffer
+    "j"   'swiper-avy
+    "y"   'yas-insert-snippet
+    "q"   'kill-emacs
+    "r"   'reload-current-buffer
+    "?"   'flyspell-correct-at-point
+    "!"   'flyspell-add-word-to-dict
+    "0"   'switch-window
     "TAB" 'indent-region
     "RET" 'eval-buffer))
 
@@ -144,6 +167,31 @@
   :config
   (evil-mode t))
 
+(setq evil-want-C-u-scroll t)
+(setq evil-want-integration t)
+(setq evil-want-keybinding nil)
+
+(use-package evil-collection
+  :straight t
+  :after evil
+  :config
+  (setq evil-collection-setup-minibuffer t)
+  (setq evil-collection-company-use-tng nil)
+  (evil-collection-init))
+
+(use-package evil-magit
+  :straight t
+  :after evil
+  :config
+  (setq evil-magit-use-y-for-yank t))
+
+;; provides gl and gL align operators
+(use-package evil-lion
+  :straight t
+  :after evil
+  :config
+  (evil-lion-mode))
+
 ;; counsel
 (use-package counsel
   :straight t
@@ -152,7 +200,11 @@
    ("C-c g" . counsel-ag)
    ("C-x C-f" . counsel-find-file)))
 
-;; ivy
+(use-package counsel-projectile
+  :straight t
+  :after projectile)
+
+;; ivy - generic completion frontend
 (use-package ivy
   :straight t
   :diminish ivy-mode
@@ -171,6 +223,56 @@
   :straight t
   :config
   (ivy-rich-mode t))
+
+(use-package all-the-icons-ivy
+  :straight t)
+
+(use-package all-the-icons-ivy-rich
+  :straight t
+  :config
+  (all-the-icons-ivy-rich-mode t))
+
+;; swiper - isearch replacement
+(use-package swiper
+  :straight t
+  :bind
+  (("C-s" . swiper)))
+
+;; avy - jump to characters and expressions fast
+(use-package avy
+  :straight t
+  :custom-face
+  :config
+  (setq avy-background t)
+  (setq avy-all-windows t)
+  (custom-set-faces
+   '(avy-lead-face
+     ((t (:inherit avy-lead-face
+                   :background "#eeeeee"
+                   :foreground "#424242"))))
+   '(avy-lead-face-0
+     ((t (:inherit avy-lead-face-0
+                   :background "#feca32"
+                   :foreground "#424242")))))
+  :bind
+  (("C-c a" . avy-goto-char)
+   ("C-c o" . avy-goto-char-timer)
+   ("C-c e" . swiper-avy)))
+
+;; frog-jumping
+(use-package frog-jump-buffer
+  :straight t)
+
+;; Project management
+(use-package projectile
+  :straight t
+  :diminish projectile-mode
+  :config
+  (projectile-mode t))
+
+;; Smart M-x enhancement, e.g. sorting items in M-x minibuffer by usage
+(use-package smex
+  :straight t)
 
 ;; company autocompletion
 (use-package company
@@ -214,16 +316,84 @@
   (setq magit-diff-paint-whitespace t)
   (setq magit-completion-read-function 'ivy-completion-read))
 
-(use-package evil-magit
-  :straight t
-  :after evil
-  :config
-  (setq evil-magit-use-y-for-yank t))
-
 ;; Switch window
 (use-package switch-window
   :straight t)
 
+;; Indentation defaults
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+(setq-default c-basic-offset 2)
+(setq-default js-indent-level 2)
+
+;; Execute path from shell
+(use-package exec-path-from-shell
+  :straight t
+  :config
+  (exec-path-from-shell-initialize))
+
+;; Snippet system
+(use-package yasnippet
+  :straight t
+  :config
+  (setq yas-trigger-in-field t)
+  (setq yas-snippet-dirs
+        '("~/.emacs.d/snippets"))
+  (yas-global-mode t))
+
+(use-package yasnippet-snippets
+  :straight t
+  :after yasnippet)
+
+;; Undo visualization
+(use-package undo-tree
+  :straight t
+  :diminish undo-tree-mode
+  :init
+  (global-undo-tree-mode))
+
+;; Automatically highlight symbols matching the current selection
+(use-package auto-highlight-symbol
+  :straight t
+  :hook (prog-mode . auto-highlight-symbol-mode))
+
+;; Hydra
+(use-package hydra
+  :straight t)
+
+;; Rainbow delimiters, color highlight parenthesis
+(use-package rainbow-delimiters
+  :straight t
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; Color coded hex/rgb/hls values
+(use-package rainbow-mode
+  :straight t
+  :diminish rainbow-mode
+  :hook (prog-mode . rainbow-mode))
+
+;; Smart hungry delete
+(use-package smart-hungry-delete
+  :straight t
+  :bind
+  (("C-<backspace>" . smart-hungry-delete-backward-char)
+   ("C-d" . smart-hungry-delete-forward-char))
+  :config
+  (smart-hungry-delete-add-default-hooks))
+
+;; Load custom functions
+(add-to-list 'load-path "~/.emacs.d/core/")
+
+(require 'core.functions)
+
+;; Reset garbage collection. Not doing so will cause garbage
+;; collection freezes during long-term interactive use. Conversely, a
+;; gc-cons-threshold that is too small will cause stuttering. We use 16mb as our
+;; default.
+(add-hook 'emacs-startup-hook
+  (lambda ()
+    (setq gc-cons-threshold 33554432 ; 32mb
+          gc-cons-percentage 0.1)))
 
 (provide 'init.el)
 ;;; init.el ends here
