7a885bf29dfebc93a1b0660d4d0f9c01fa662f0b
Guido Schmidt <git@guidoschmidt.cc>
2021-01-09 15:58:51 +0100

    EDIT/init.el: add lsp-ui-doc-glance to lsp-ui hydra

--- init.el
+++ init.el
@@ -585,15 +585,18 @@ LSP UI
 _d_: find definitions
 _x_: go to
 _r_: find references
+_?_: show documentation
 
 _h_: ← backwards
 _l_: → forwards
 "
     ("d" lsp-ui-peek-find-definitions "find definitions")
     ("x" lsp-ui-peek--goto-xref       "go to")
+    ("r" lsp-ui-peek-find-references  "references")
+    ("?" lsp-ui-doc-glance            "documentation")
+
     ("h" lsp-ui-peek-jump-backward    "<")
-    ("l" lsp-ui-peek-jump-forward     ">")
-    ("r" lsp-ui-peek-find-references  "references")))
+    ("l" lsp-ui-peek-jump-forward     ">")))
 
 (use-package lsp-ivy
   :straight t)
@@ -663,6 +666,21 @@ _l_: → forwards
   :mode "\\.js\\'"
   :init
   (setq-default rjsx-indent-level 2))
+
+(defvar flycheck-javascript-eslint-executable)
+(defun custom/use-eslint-from-node-modules ()
+  "Use local eslint from node_modules before global.
+src: http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable"
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (if (and eslint (file-executable-p eslint))
+        (setq-local flycheck-javascript-eslint-executable eslint)
+      (setq-local flycheck-javascript-eslint-executable
+                  "~/.nvm/versions/node/v15.4.0/bin/eslint"))))
 
 (use-package react-snippets
   :straight t)
