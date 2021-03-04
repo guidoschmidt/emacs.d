68323748c64ead5871fa795c15bb9929ca29b8f4
Guido Schmidt <git@guidoschmidt.cc>
2021-03-04 10:28:58 +0100

    EDIT/feat.org.el: setup org mode

---
+++ features/feat.org.el
@@ -0,0 +1,21 @@
+;;; feat.org.el --- Org mode setup -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Org mode configuration and setup
+
+;;; Code:
+
+;;; Hide the emphasis markup
+;;; (e.g. /.../ for italics, *...* for bold, etc.)
+(setq org-hide-emphasis-markers t)
+
+(use-package org-bullets
+  :straight t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+
+
+(provide 'feat.org)
+;;; feat.org.el ends here
