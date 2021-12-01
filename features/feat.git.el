aeacef66fa5ec4804291130042c9b26dc50193a1
Guido Schmidt <git@guidoschmidt.cc>
2021-12-01 15:48:18 +0100

    EDIT/features/feat.git.el: blamer mode

--- features/feat.git.el
+++ features/feat.git.el
@@ -10,5 +10,20 @@
   (setq magit-diff-paint-whitespace t)
   (setq magit-completion-read-function 'ivy-completion-read))
 
+(use-package blamer
+  :straight (blamer
+             :type git
+             :host github
+             :repo "Artawower/blamer.el")
+  :defer 20
+  :custom
+  (blamer-idle-time 0.3)
+  (blamer-min-offset 70)
+  :custom-face
+  (blamer-face ((t :foreground "#7a88cf"
+                   :background nil
+                   :height 140
+                   :italic t))))
+
 (provide 'feat.git)
 ;;; feat.git.el ends here
