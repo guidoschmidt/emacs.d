76ab66f04c79d07dfce1a6a85c30d93db47626ea
Guido Schmidt <git@guidoschmidt.cc>
2021-12-18 20:41:25 +0100

    ADD/languages/lang.graphviz.el

---
+++ languages/lang.graphviz.el
@@ -0,0 +1,18 @@
+;;; lang.graphviz.el --- Graphviz support -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Use graphviz dot files in Emacs
+
+;;; Code:
+(use-package graphviz-dot-mode
+  :straight (graphviz-dot-mode
+             :type git
+             :host github
+             :repo "ppareit/graphviz-dot-mode")
+  :config
+  (setq graphviz-dot-intend-with 4)
+  (use-package company-graphviz-dot))
+
+(provide 'lang.graphviz)
+;;; lang.graphviz.el ends here
