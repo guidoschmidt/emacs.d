4211c4a8833a1a0bd4d7c417b364d4ce0f7b9903
Guido Schmidt <git@guidoschmidt.cc>
2022-01-30 14:31:17 +0100

    FIX/languages/lang.clojure.el: cider configuration

--- languages/lang.clojure.el
+++ languages/lang.clojure.el
@@ -9,7 +9,18 @@
   :straight t)
 
 (use-package cider
-  :straight t)
+  :straight t
+  :config
+  (setq cider-repl-history-file ".cider-repl-history"
+        nrepl-log-messages t))
+
+(use-package flycheck-clojure
+  :straight t
+  :config
+  (flycheck-clojure-setup)
+  (eval-after-load 'flycheck
+    '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
+  (add-hook 'after-init-hook #'global-flycheck-mode))
 
 (provide 'lang.clojure)
 ;;; lang.clojure.el ends here
