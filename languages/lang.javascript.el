2e2d9cc22effb397acdd0bcb4d01b11f3855d83b
Guido Schmidt <git@guidoschmidt.cc>
2022-09-17 10:12:50 +0200

    EDIT/languages/lang.javascript.el: set prettier path for Vreni

--- languages/lang.javascript.el
+++ languages/lang.javascript.el
@@ -44,7 +44,7 @@ src: http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-
   (when (hostname? "gs-cube")
     (setq prettier-js-command "~/.nvm/versions/node/v17.1.0/bin/prettier"))
   (when (hostname? "Vreni")
-    (setq prettier-js-command "~/.nvm/versions/node/v17.0.1/bin/prettier"))
+    (setq prettier-js-command "/usr/local/bin/prettier"))
   (when (hostname? "Brandon.fritz.box")
     (setq prettier-js-command "~/.nvm/versions/node/v16.9.1/bin/prettier"))
   (when (hostname? "Brandon.local")
