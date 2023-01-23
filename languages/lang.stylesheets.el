ab2531ec404b27ae6ba944c6a0a119726e83c893
Guido Schmidt <git@guidoschmidt.cc>
2023-01-23 22:45:32 +0100

    EDIT/lang.stylesheets.el: improve scss mode

--- languages/lang.stylesheets.el
+++ languages/lang.stylesheets.el
@@ -13,12 +13,12 @@
     "Hook to customize SASS mode."
     (setq rainbow-html-colors t)
     (rainbow-mode)
-    (setq emmet-use-sass-syntax t))
-  (defun sass-scss-company-mode-hook ()
+    (setq emmet-use-sass-syntax t)
     (setq-local company-backends
                 '((company-files :with company-dabbrev company-yasnippet))))
   (add-hook 'scss-mode-hook #'sass-scss-company-mode-hook)
-  :hook '(sass-mode . custom/sass-mode-hook))
+  :hook ((sass-mode . custom/sass-mode-hook)
+         (scss-mode . custom/sass-mode-hook)))
 
 (provide 'lang.stylesheets)
 ;;; lang.stylesheets.el ends here
