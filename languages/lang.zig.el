c1687099647e14e4127ab040e628815dbff29536
Guido Schmidt <git@guidoschmidt.cc>
2023-05-04 09:19:23 +0200

    EDIT/languages/lang.zig.el: setup zls on Cube

--- languages/lang.zig.el
+++ languages/lang.zig.el
@@ -12,7 +12,10 @@
   :custom (zig-format-on-save nil)
   :config
   (add-to-list 'lsp-language-id-configuration '(zig-mode . "zig"))
-  (setq lsp-zig-zls-executable "~/git/zig/zls/zig-out/bin/zls"))
+  (when (hostname? "Cube")
+    (setq lsp-zig-zls-executable "~/.zig/zls.exe"))
+  (when (hostname? "Vreni")
+    (setq lsp-zig-zls-executable "~/git/zig/zls/zig-out/bin/zls")))
 
 (provide 'lang.zig)
 ;;; lang.zip.el ends here
