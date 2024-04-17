6eaaaca3060d84c21f5d042716d532be364aa818
Guido Schmidt <git@guidoschmidt.cc>
2024-04-17 02:00:45 +0200

    EDIT/lang.zig.el: zig indent + colorization of compilation buffer

--- languages/lang.zig.el
+++ languages/lang.zig.el
@@ -11,6 +11,14 @@
   :hook ((zig-mode . lsp-deferred))
   :custom (zig-format-on-save nil)
   :config
+  (setq zig-indent-offset 4)
+  (if (>= emacs-major-version 28)
+      (add-hook 'compilation-filter-hook 'ansi-color-compilation-filter)
+    (progn
+      (defun colorize-compilation-buffer ()
+        (let ((inhibit-read-only t))
+          (ansi-color-apply-on-region compilation-filter-start (point))))
+      (add-hook 'compilation-filter-hook 'colorize-compilation-buffer)))
   (add-to-list 'lsp-language-id-configuration '(zig-mode . "zig"))
   (when (hostname? "Cube")
     (setq lsp-zig-zls-executable "~/.zig/zls.exe"))
