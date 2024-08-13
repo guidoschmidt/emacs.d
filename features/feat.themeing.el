08524d39cc904988c717f3147a9dccf0f8b689d0
Guido Schmidt <git@guidoschmidt.cc>
2024-08-13 16:13:31 +0200

    feat: update snipptes/ submodule

--- features/feat.themeing.el
+++ features/feat.themeing.el
@@ -54,8 +54,8 @@
   (setq circadian-verbose t)
   (setq calendar-latitude 49.398750)
   (setq calendar-longitude 8.672434)
-  (setq circadian-themes '((:sunrise . (doom-gruvbox-light
-                                        doom-oksolar-light))
+  (setq circadian-themes '((:sunrise . (kaolin-temple
+                                        kaolin-valley-dark))
                            (:sunset  . (doom-dracula
                                         doom-badger
                                         doom-feather-dark))
