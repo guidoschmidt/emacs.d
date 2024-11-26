3fd9326aff1dc285f40fd5cddab0afc2eaa84c5e
Guido Schmidt <git@guidoschmidt.cc>
2024-11-26 22:23:46 +0100

    feat(lang.python.el): add ruff lsp

--- languages/lang.python.el
+++ languages/lang.python.el
@@ -7,22 +7,35 @@
 ;;; Code:
 (use-package python-mode
   :straight t
-  :mode "\\py\\'"
+  :defer python-mode
   :hook ((python-mode . lsp-deferred))
+  :after lsp-mode
   :config
   (when (hostname? "Vreni")
     (setq-default python-shell-interpreter "~/.pyenv/versions/3.7.2/bin/python3"))
   (when (hostname? "Cube")
     (setq-default python-shell-interpreter "C:/Program Files/Python311/python.exe"))
-  ;; LSP config for ruff
+
+  ;; LSP config with ruff
   ;; Use shopify-cli / theme-check-language-server for Shopify's liquid syntax.
   (with-eval-after-load 'lsp-mode
+    (add-to-list 'lsp-disabled-clients 'pyls)
     (add-to-list 'lsp-language-id-configuration
                  '(python-mode . "python"))
     (lsp-register-client
      (make-lsp-client :new-connection (lsp-stdio-connection "ruff")
                       :activation-fn (lsp-activate-on "python")
-                      :server-id 'ruff))))
+                      :priority -3
+                      :server-id 'ruff)))
+
+  ;; dap-mode config
+  (require 'dap-python)
+  (setq dap-python-debugger 'debugpy)
+  (defun dap-python--pyenv-executable-find (command)
+    (with-venv (executable-find "python")))
+
+  (add-hook 'dap-stopped-hook
+            (lambda (arg) (call-interactively #'dap-hydra))))
 
 (use-package pippel
   :straight t)
